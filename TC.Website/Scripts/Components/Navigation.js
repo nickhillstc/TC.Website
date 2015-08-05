42922719dc0013c34a3a69902e413db50488ae15
gothandy <gothandy@hotmail.com>
2015-08-05 18:23:27 +0100

    Remaining website files.

---
+++ TC.Website/Scripts/Components/Navigation.js
@@ -0,0 +1,13 @@
+﻿// Nav
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() < 25 && $('#header_nav').hasClass('transparent')) {
+        $('#header_nav').removeClass('tiny');
+    } else {
+        $('#header_nav').addClass('tiny');
+    }
+});
+
+if (!$('#header_nav').hasClass('transparent')) $('#header_nav').addClass('tiny');
+
+
